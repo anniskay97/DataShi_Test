4314d2751ef5e2ca44f5347b44406a9d79d84a92
Anna <anniskay97@users.noreply.github.com>
2018-10-01 00:16:54 +0300

    Added server

---
+++ server/erverPost/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using erverPost.Models;
+
+namespace erverPost.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // POST api/values
+        [HttpPost]
+        public IActionResult Post([FromBody]User value)
+        {
+            return Ok(value);
+        }
+    }
+}
